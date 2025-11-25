14847d396a16e2823ce18083cd9ca0455b303785
yang491658 <park49164357@gmail.com>
2025-11-26 07:45:32 +0900

    수정
    - 카운트 다운 추가
    - 에디터 버튼 추가 (테스트/종료/빌드)

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -50,10 +50,9 @@ public class GameManager : MonoBehaviour
         Pause(false);
         IsGameOver = false;
 
-        SoundManager.Instance?.PlayBGM("Default");
-
         UIManager.Instance?.ResetUI();
         UIManager.Instance?.OpenUI(false);
+        UIManager.Instance?.StartCountdown();
 
         EntityManager.Instance?.CancelRespawn();
         EntityManager.Instance?.ResetCount();
